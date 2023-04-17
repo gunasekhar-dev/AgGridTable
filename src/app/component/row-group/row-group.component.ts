7a9d02008a5481b6cf1692dd5bafcd29578852f5
gunasekhar-dev <80669471+gunasekhar-dev@users.noreply.github.com>
2023-04-17 19:40:44 +0530

    gridcommit

--- src/app/component/row-group/row-group.component.ts
+++ src/app/component/row-group/row-group.component.ts
@@ -1,63 +1,51 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IOlympicData } from '../interfaces';
-import { Grid, GridOptions, ValueGetterParams } from 'ag-grid-community';
+import {
+  ColDef,
+  Grid,
+  GridApi,
+  GridOptions,
+  GridReadyEvent,
+  ValueGetterParams,
+} from 'ag-grid-community';
 import 'ag-grid-enterprise';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-row-group',
   templateUrl: './row-group.component.html',
   styleUrls: ['./row-group.component.css'],
 })
-export class RowGroupComponent implements OnInit {
-  grid: any;
-  ngOnInit(): void {
-    var gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
-    new Grid(gridDiv, this.gridOptions);
-    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
-      .then((response) => response.json())
-      .then((data: IOlympicData[]) => this.gridOptions.api!.setRowData(data));
+export class RowGroupComponent {
+  private gridApi!: GridApi<IOlympicData>;
 
-    if (typeof window !== 'undefined') {
-      // Attach external event handlers to window so they can be called from index.html
-      (<any>window).applyFilter = this.applyFilter;
-    }
-  }
-
-  gridOptions: GridOptions<IOlympicData> = {
-    columnDefs: [
-      { field: 'sport', rowGroup: true, hide: true },
-      { field: 'country', rowGroup: true, hide: true },
-      { field: 'gold', aggFunc: 'avg' },
-      { field: 'silver', aggFunc: 'sum' },
-      { field: 'bronze', aggFunc: 'sum' },
-    ],
-    defaultColDef: {
-      flex: 1,
-      minWidth: 150,
-      filter: true,
-      floatingFilter: true,
-      resizable: true,
-      sortable: true,
-    },
-    autoGroupColumnDef: {
-      minWidth: 260,
-      filter: 'agTextColumnFilter',
-      filterValueGetter: (params: ValueGetterParams) => {
-        const colId = params.column.getColId();
-        if (colId.includes('sport')) {
-          return params.data.sport;
-        } else if (colId.includes('country')) {
-          return params.data.country;
-        }
-      },
-    },
-    groupDisplayType: 'multipleColumns',
-    animateRows: true,
+  public columnDefs: ColDef[] = [
+    { field: 'sport', rowGroup: true, hide: true },
+    { field: 'country', rowGroup: true, hide: true },
+    { field: 'gold', aggFunc: 'sum' },
+    { field: 'silver', aggFunc: 'sum' },
+    { field: 'bronze', aggFunc: 'sum' },
+  ];
+  public defaultColDef: ColDef = {
+    flex: 1,
+    minWidth: 150,
+    filter: true,
+    sortable: true,
+    floatingFilter: true,
+    resizable: true,
   };
+  public autoGroupColumnDef: ColDef = {
+    minWidth: 260,
+    filter: 'agTextColumnFilter',
+    filterValueGetter: (params: ValueGetterParams) => params.data.sport,
+  };
+  public rowData!: IOlympicData[];
+
+  constructor(private http: HttpClient) {}
 
   applyFilter() {
-    this.gridOptions.api!.setFilterModel({
-      'ag-Grid-AutoColumn-sport': {
+    this.gridApi.setFilterModel({
+      'ag-Grid-AutoColumn': {
         filterType: 'text',
         type: 'contains',
         filter: 'Skiing',
@@ -65,5 +53,14 @@ export class RowGroupComponent implements OnInit {
     });
   }
 
+  onGridReady(params: GridReadyEvent<IOlympicData>) {
+    this.gridApi = params.api;
+
+    this.http
+      .get<IOlympicData[]>(
+        'https://www.ag-grid.com/example-assets/olympic-winners.json'
+      )
+      .subscribe((data) => (this.rowData = data));
+  }
   // setup the grid after the page has finished loading
 }
